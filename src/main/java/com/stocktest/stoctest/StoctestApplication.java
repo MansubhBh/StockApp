b79aa696e57c81f7d5b26eaa95e9f9d973729f49
Mansubh Bhandari <kbhandary69@gmail.com>
2018-06-01 18:44:10 +0545

    first commit, basic project structure

---
+++ src/main/java/com/stocktest/stoctest/StoctestApplication.java
@@ -0,0 +1,12 @@
+package com.stocktest.stoctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoctestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stocktest/stoctest/StoctestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stocktest.stoctest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoctestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
